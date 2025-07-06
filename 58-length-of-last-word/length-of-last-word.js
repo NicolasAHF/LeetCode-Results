db5c9b75b16d0a2df896c4df455e5183edaeca49
Nicolas Hernandez <69226118+NicolasAHF@users.noreply.github.com>
2025-07-06 18:34:43 -0300

    Time: 0 ms (100.00%) | Memory: 53.9 MB (32.54%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.js
+++ 58-length-of-last-word/length-of-last-word.js
@@ -3,15 +3,7 @@
  * @return {number}
  */
 var lengthOfLastWord = function(s) {
-    let aux = s.split(" ");
-    let k = 0;
-    let result = []
-    for(let i = 0; i < aux.length; i++){
-        if(aux[i] != ""){
-            result[k] = aux[i];
-            k++;
-        }
-    }
-    console.log(result);
+    let result = s.split(" ");
+    result = result.filter(word => word != "");
     return result[result.length - 1].length
 };
