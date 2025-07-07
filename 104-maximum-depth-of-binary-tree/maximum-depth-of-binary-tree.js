4d2ac0ce28414aca0c7aac4ec7d07460a5d2f170
Nicolas Hernandez <69226118+NicolasAHF@users.noreply.github.com>
2025-07-07 11:08:32 -0300

    Time: 0 ms (100.00%) | Memory: 58.8 MB (22.25%) - LeetSync

---
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.js
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function(root) {
+    let countIzq = 0;
+    let countDer = 0;
+    if(root === null){
+        return 0;
+    }else{
+        if(root.left === null && root.right === null){
+        return 1;
+    }else{
+        if(root.left){
+            countIzq = maxDepth(root.left) + 1;
+        }
+        if(root.right){
+            countDer = maxDepth(root.right) + 1;
+        }
+    }
+    }
+    
+    if(countIzq > countDer){
+        return countIzq;
+    }else{
+        return countDer;
+    }
+};
