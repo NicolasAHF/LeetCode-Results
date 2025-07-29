dc0b16bb16ce4aed520a8fd2b544272aedcf1f2e
Nicolas Hernandez <69226118+NicolasAHF@users.noreply.github.com>
2025-07-29 15:57:12 -0300

    Time: 0 ms (100.00%) | Memory: 53.7 MB (42.36%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.js
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.js
@@ -3,6 +3,16 @@
  * @return {number}
  */
 var findMin = function(nums) {
-    nums.sort((a, b) => a - b);
-    return nums[0];
+    let left = 0;
+    let rigth = nums.length - 1;
+     while(left < rigth){
+        const mid = Math.floor((left + rigth) / 2);
+
+        if(nums[mid] > nums[rigth]){
+            left = mid + 1;
+        }else{
+            rigth = mid;
+        }
+     }
+    return nums[left]
 };
