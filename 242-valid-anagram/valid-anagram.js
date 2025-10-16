fa47b9550df55ca2764fd93cfe7fe984eb4ce831
Nicolas Hernandez <69226118+NicolasAHF@users.noreply.github.com>
2025-10-16 16:13:04 -0300

    Time: 30 ms (20.40%) | Memory: 58 MB (55.12%) - LeetSync

--- 242-valid-anagram/valid-anagram.js
+++ 242-valid-anagram/valid-anagram.js
@@ -7,16 +7,8 @@ var isAnagram = function(s, t) {
     if(s.length !== t.length){
         return false;
     }
-    const obj = new Map();
-    for(char of s){
-        obj.set(char, (obj.get(char) || 0) + 1);
-    }
+    let sAux = s.split('').sort().join('');
+    let tAux = t.split('').sort().join('');
 
-    for(char of t){
-        if(!obj.has(char) || obj.get(char) === 0){
-            return false;
-        }
-        obj.set(char, obj.get(char) - 1);
-    }
-    return true;
+    return sAux == tAux
 };
